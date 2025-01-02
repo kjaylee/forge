626a15628bbf14b04eac3b523f1d10f72ed6c80b
laststylebender <ran.mahadik14@gmail.com>
2025-01-02 16:14:49 +0530

    - add tests

--- crates/forge_server/src/service/neo_chat_service.rs
+++ crates/forge_server/src/service/neo_chat_service.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use forge_provider::{
-    CompletionMessage, FinishReason, ProviderService, Request, ResultStream, ToolCall,
+    CompletionMessage, FinishReason, ProviderService, Request, ResultStream, ToolCall, ToolResult,
 };
 use forge_tool::ToolService;
 use tokio_stream::wrappers::ReceiverStream;
@@ -62,8 +62,15 @@ impl Live {
                         .await
                         .expect("Failed to send message");
                 } else {
-                    // we start the folding process.
                     if let Some(tool_part) = message.tool_call.first() {
+                        if current_tool.is_empty() {
+                            // very first instance where we found a tool call.
+                            if let Some(tool_name) = &tool_part.name {
+                                tx.send(Ok(ChatResponse::ToolUseStart(tool_name.clone().into())))
+                                    .await
+                                    .expect("Failed to send message");
+                            }
+                        }
                         current_tool.push(tool_part.clone());
                     }
 
@@ -82,8 +89,12 @@ impl Live {
             let assitant_message = "".to_string();
             request = request.add_message(CompletionMessage::assistant(assitant_message));
             if let Some(Ok(tool_result)) = tool_result {
-                let tool_result = serde_json::from_value(tool_result).unwrap();
-                request = request.add_message(CompletionMessage::ToolMessage(tool_result));
+                let tool_result: ToolResult = serde_json::from_value(tool_result).unwrap();
+                request = request.add_message(CompletionMessage::ToolMessage(tool_result.clone()));
+                // send the tool use end message.
+                tx.send(Ok(ChatResponse::ToolUseEnd(tool_result)))
+                    .await
+                    .expect("Failed to send message");
             } else {
                 break Ok(());
             }
@@ -121,8 +132,9 @@ pub mod tests {
 
     use derive_setters::Setters;
     use forge_provider::{
-        CompletionMessage, Error, Model, ModelId, Parameters, ProviderError, ProviderService,
-        Request, Response, Result, ResultStream,
+        CompletionMessage, Error, FinishReason, Model, ModelId, Parameters, ProviderError,
+        ProviderService, Request, Response, Result, ResultStream, ToolCallId, ToolCallPart,
+        ToolResult,
     };
     use forge_tool::{ToolDefinition, ToolName, ToolService};
     use pretty_assertions::assert_eq;
@@ -136,7 +148,7 @@ pub mod tests {
 
     #[derive(Default, Setters)]
     pub struct TestProvider {
-        messages: Vec<Response>,
+        messages: Mutex<Vec<Vec<Response>>>,
         request: Mutex<Option<Request>>,
         models: Vec<Model>,
         parameters: Vec<(ModelId, Parameters)>,
@@ -182,7 +194,11 @@ pub mod tests {
             // TODO: don't remove this else tests stop working, but we need to understand
             // why so revisit this later on.
             tokio::time::sleep(tokio::time::Duration::from_millis(5)).await;
-            Ok(Box::pin(tokio_stream::iter(self.messages.clone()).map(Ok)))
+
+            // clear the messages as we send it to the stream.
+            let mut guard = self.messages.lock().unwrap();
+            let response = guard.remove(0);
+            Ok(Box::pin(tokio_stream::iter(response).map(Ok)))
         }
 
         async fn models(&self) -> Result<Vec<Model>> {
@@ -200,18 +216,59 @@ pub mod tests {
         }
     }
 
+    const ASSISTANT_RESPONSE: &str = "Sure thing!";
+    const SYSTEM_PROMPT: &str = "Do everything that the user says";
+
+    struct FixtureBuilder {
+        provider: Arc<TestProvider>,
+        system_prompt: Arc<TestSystemPrompt>,
+        tool: Arc<TestToolService>,
+        service: Live,
+    }
+
+    impl FixtureBuilder {
+        pub fn with_provider(self, provider: TestProvider) -> Self {
+            let provider = Arc::new(provider);
+            Self {
+                provider: provider.clone(),
+                service: Live::new(provider, self.system_prompt.clone(), self.tool.clone()),
+                system_prompt: self.system_prompt,
+                tool: self.tool,
+            }
+        }
+
+        pub fn with_tool(self, tool_result: ToolResult) -> Self {
+            let tool = Arc::new(TestToolService::new(
+                serde_json::to_value(tool_result).unwrap(),
+            ));
+            Self {
+                provider: self.provider.clone(),
+                service: Live::new(self.provider, self.system_prompt.clone(), tool.clone()),
+                system_prompt: self.system_prompt,
+                tool,
+            }
+        }
+    }
+
+    impl Default for FixtureBuilder {
+        fn default() -> Self {
+            let provider = Arc::new(TestProvider::default().messages(Mutex::new(vec![vec![
+                Response::assistant(ASSISTANT_RESPONSE.to_string()),
+            ]])));
+            let system_prompt = Arc::new(TestSystemPrompt::new(SYSTEM_PROMPT));
+            let tool = Arc::new(TestToolService::new(json!({"result": "fs success."})));
+            let service = Live::new(provider.clone(), system_prompt.clone(), tool.clone());
+            Self { provider, system_prompt, tool, service }
+        }
+    }
+
     #[tokio::test]
     async fn test_chat_response() {
-        let message = "Sure thing!".to_string();
-        let provider =
-            Arc::new(TestProvider::default().messages(vec![Response::assistant(message.clone())]));
-        let system_prompt = Arc::new(TestSystemPrompt::new("Do everything that the user says"));
-        let tool = Arc::new(TestToolService::new(json!({"result": "fs success."})));
-        let service = Live::new(provider.clone(), system_prompt, tool);
-
+        let tester = FixtureBuilder::default();
         let chat_request = ChatRequest::new("Hello can you help me?");
 
-        let actual = service
+        let actual = tester
+            .service
             .chat(chat_request)
             .await
             .unwrap()
@@ -221,32 +278,65 @@ pub mod tests {
             .map(|value| value.unwrap())
             .collect::<Vec<_>>();
 
-        let expected = vec![ChatResponse::Text(message)];
+        let expected = vec![ChatResponse::Text(ASSISTANT_RESPONSE.to_string())];
 
         assert_eq!(actual, expected)
     }
 
     #[tokio::test]
     async fn test_chat_system_prompt() {
-        let message = "Sure thing!".to_string();
-        let provider =
-            Arc::new(TestProvider::default().messages(vec![Response::assistant(message.clone())]));
-        let system_message = "Do everything that the user says";
-        let system_prompt = Arc::new(TestSystemPrompt::new(system_message));
-        let tool = Arc::new(TestToolService::new(json!({"result": "fs success."})));
-        let service = Live::new(provider.clone(), system_prompt, tool);
-
+        let tester = FixtureBuilder::default();
         let chat_request = ChatRequest::new("Hello can you help me?");
 
         // TODO: don't remove this else tests stop working, but we need to understand
         // why so revisit this later on.
         tokio::time::pause();
-        let _ = service.chat(chat_request).await.unwrap();
+        let _ = tester.service.chat(chat_request).await.unwrap();
         tokio::time::advance(tokio::time::Duration::from_millis(5)).await;
 
-        let actual = provider.get_last_call().unwrap().messages[0].clone();
-        let expected = CompletionMessage::system(system_message);
+        let actual = tester.provider.get_last_call().unwrap().messages[0].clone();
+        let expected = CompletionMessage::system(SYSTEM_PROMPT.to_string());
 
         assert_eq!(actual, expected)
+    }
+
+    #[tokio::test]
+    async fn test_chat_tool_result() {
+        let message_1 = Response::new("Let's use foo tool").add_tool_call(
+            ToolCallPart::default()
+                .name(ToolName::new("foo"))
+                .arguments_part(r#"{"foo": 1,"#)
+                .call_id(ToolCallId::new("too_call_001")),
+        );
+        let message_2 = Response::new("")
+            .add_tool_call(ToolCallPart::default().arguments_part(r#""bar": 2}"#))
+            .finish_reason(FinishReason::ToolCalls);
+        let message_3 = Response::new("Task is complete, let me know how can i help you!");
+
+        let tool_result = ToolResult::new(ToolName::new("foo")).content(json!({
+            "a": 100,
+            "b": 200
+        }));
+
+        let tester = FixtureBuilder::default()
+            .with_provider(TestProvider::default().messages(Mutex::new(vec![
+                vec![message_1, message_2],
+                vec![message_3.clone()],
+            ])))
+            .with_tool(tool_result.clone());
+
+        let result = tester
+            .service
+            .chat(ChatRequest::new("Hello can you help me?"))
+            .await
+            .unwrap()
+            .collect::<Vec<_>>()
+            .await
+            .into_iter()
+            .map(|value| value.unwrap())
+            .collect::<Vec<_>>();
+        assert!(result.contains(&ChatResponse::ToolUseStart(ToolName::new("foo").into())));
+        assert!(result.contains(&ChatResponse::ToolUseEnd(tool_result)));
+        assert!(result.contains(&ChatResponse::Text(message_3.content)));
     }
 }
