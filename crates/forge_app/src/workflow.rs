3e8c950373eeb81595aff8536d98e187b7cf0324
laststylebender <ran.mahadik14@gmail.com>
2025-02-10 21:16:43 +0530

    - add coding agent

--- crates/forge_app/src/workflow.rs
+++ crates/forge_app/src/workflow.rs
@@ -3,11 +3,13 @@ use forge_domain::{
 };
 
 struct Workflow {
+    coding_agent: Agent,
     learning_extractor_agent: Agent,
     learning_finder_agent: Agent,
 }
 
 impl Workflow {
+    // TODO: figure out better way to add tools for specific agent.
     pub fn new(env: Environment) -> Self {
         let mut agent = AgentBuilder::default();
         agent
@@ -30,6 +32,7 @@ impl Workflow {
             .unwrap(); // add tool that helps us to save the learnings.
 
         let learning_finder_agent = agent
+            .clone()
             .id(AgentId::new("learning-finder"))
             .system_prompt(Prompt::<SystemContext>::new(include_str!(
                 "./prompts/learning_finder.md"
@@ -40,6 +43,21 @@ impl Workflow {
             .build()
             .unwrap();
 
-        Self { learning_extractor_agent, learning_finder_agent }
+        let coding_agent = agent
+            .id(AgentId::new("coding-agent"))
+            .system_prompt(Prompt::<SystemContext>::new(include_str!(
+                "./prompts/coding/system.md"
+            )))
+            .user_prompt(Prompt::<Variables>::new("<task>{{task}}</task"))
+            .provider(Provider::new("open-ai"))
+            .tools(vec![])
+            .build()
+            .unwrap();
+
+        Self {
+            learning_extractor_agent,
+            learning_finder_agent,
+            coding_agent,
+        }
     }
 }
