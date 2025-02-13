bb750f8e2a62e51056a5600bba597a59245ee8be
laststylebender <ran.mahadik14@gmail.com>
2025-02-13 10:48:41 +0530

    - remove unsupported commands

--- crates/forge_main/src/model.rs
+++ crates/forge_main/src/model.rs
@@ -86,13 +86,9 @@ impl Command {
         vec![
             "/end".to_string(),
             "/new".to_string(),
-            "/reload".to_string(),
             "/info".to_string(),
             "/exit".to_string(),
             "/models".to_string(),
-            "/config".to_string(),
-            "/config set".to_string(),
-            "/config get".to_string(),
         ]
     }
 
