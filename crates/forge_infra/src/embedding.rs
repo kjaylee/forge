c5979dfa58717ae1f17aa91167204b42ed171746
Tushar Mathur <tusharmath@gmail.com>
2025-02-16 13:48:44 -0800

    feat: add rust-bert dependency and implement embedding functionality in ForgeEmbeddingService

--- crates/forge_infra/src/embedding.rs
+++ crates/forge_infra/src/embedding.rs
@@ -1,4 +1,7 @@
 use forge_app::EmbeddingService;
+use rust_bert::pipelines::sentence_embeddings::{
+    SentenceEmbeddingsBuilder, SentenceEmbeddingsModelType,
+};
 
 pub struct ForgeEmbeddingService {}
 
@@ -16,7 +19,10 @@ impl ForgeEmbeddingService {
 
 #[async_trait::async_trait]
 impl EmbeddingService for ForgeEmbeddingService {
-    async fn embed(&self, _text: &str) -> anyhow::Result<Vec<f32>> {
-        unimplemented!()
+    async fn embed(&self, sentence: &str) -> anyhow::Result<Vec<f32>> {
+        let model = SentenceEmbeddingsBuilder::remote(SentenceEmbeddingsModelType::AllMiniLmL12V2)
+            .create_model()?;
+
+        Ok(model.encode(&[sentence])?.concat())
     }
 }
