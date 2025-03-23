429b43b1f82f20d2056373ab1907f6048a82219c
Tushar Mathur <tusharmath@gmail.com>
2025-03-23 12:53:12 +0530

    feat: extend PullRequest struct to include file details and implement modified_files method

--- crates/forge_review/src/domain/model.rs
+++ crates/forge_review/src/domain/model.rs
@@ -1,21 +1,26 @@
 use serde::Serialize;
 
+/// Sample Response available at:
+/// PR https://api.github.com/repos/antinomyhq/forge/pulls/200
+/// DIFF https://patch-diff.githubusercontent.com/raw/antinomyhq/forge/pull/200.diff
 #[derive(Serialize)]
-pub struct PullRequest;
+pub struct PullRequest {
+    id: u64,
+    files: Vec<File>,
+}
 
-impl Default for PullRequest {
-    fn default() -> Self {
-        Self::new()
-    }
+/// Sample Response available at: 
+/// https://api.github.com/repos/antinomyhq/forge/pulls/200/files
+#[derive(Serialize)]
+pub struct File {
+    sha: String,
+    filename: String,
+    patch: String,
 }
 
 impl PullRequest {
-    pub fn new() -> Self {
-        Self
-    }
-
     pub fn modified_files(&self) -> Vec<String> {
-        todo!()
+        self.files.iter().map(|f| f.filename.clone()).collect()
     }
 }
 
