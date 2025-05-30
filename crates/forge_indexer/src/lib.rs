780641154170ec1cb7505593add869b5ffe3855f
laststylebender14 <ran.mahadik14@gmail.com>
2025-05-30 13:16:50 +0530

    - use cache embedder

--- crates/forge_indexer/src/lib.rs
+++ crates/forge_indexer/src/lib.rs
@@ -14,10 +14,10 @@ pub use token_counter::*;
 
 #[cfg(test)]
 mod tests {
-    use std::path::Path;
+    use std::path::{Path, PathBuf};
     use std::sync::Arc;
 
-    use crate::{FileLoader, HnswStore, OpenAI, Orchestrator, TreeSitterChunker};
+    use crate::{CachedEmbedder, FileLoader, HnswStore, OpenAI, Orchestrator, TreeSitterChunker};
 
     #[tokio::test]
     async fn test_indexer() {
@@ -25,18 +25,32 @@ mod tests {
 
         let embedding_model = "text-embedding-3-large";
         let embedding_dimensions = 1536;
+
+        let cache_dir = format!(
+            "{}:{}",
+            embedding_model.replace("/", "-"),
+            embedding_dimensions
+        );
+        let cache_path = std::env::current_dir()
+            .expect("failed to retrive current working directory.")
+            .join(PathBuf::from(format!("./cache/embeddings/{}", cache_dir)));
+
         let loader = FileLoader::default();
         let chunker = TreeSitterChunker::new(embedding_model, 8192);
-        let embedder = OpenAI::new(embedding_model, embedding_dimensions);
+        let embedder = OpenAI::cached(&cache_path, embedding_model, embedding_dimensions).unwrap();
         let hnsw_store = HnswStore::new(embedding_dimensions as usize);
 
-        let indexer: Orchestrator<FileLoader, TreeSitterChunker, OpenAI, HnswStore<'_>> =
-            Orchestrator::new(
-                Arc::new(loader),
-                Arc::new(chunker),
-                Arc::new(embedder),
-                Arc::new(hnsw_store),
-            );
+        let indexer: Orchestrator<
+            FileLoader,
+            TreeSitterChunker,
+            CachedEmbedder<OpenAI>,
+            HnswStore<'_>,
+        > = Orchestrator::new(
+            Arc::new(loader),
+            Arc::new(chunker),
+            Arc::new(embedder),
+            Arc::new(hnsw_store),
+        );
         let _ = indexer.index(Path::new("/Users/ranjit/Desktop/workspace/code-forge/code-forge/crates/forge_main/src/prompt.rs")).await.unwrap();
     }
 }
