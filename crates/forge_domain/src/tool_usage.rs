729766603a9cc3225b879b65791750ce2f88d419
laststylebender14 <ran.mahadik14@gmail.com>
2025-06-16 21:54:55 +0530

    - add nullability information in parameter

--- crates/forge_domain/src/tool_usage.rs
+++ crates/forge_domain/src/tool_usage.rs
@@ -38,21 +38,27 @@ impl Display for ToolUsagePrompt<'_> {
                 .flat_map(|(name, props)| {
                     let object = props.into_object();
                     let instance = object.instance_type.clone();
-                    object
-                        .metadata
-                        .into_iter()
-                        .map(move |meta| (name.clone(), meta, instance.clone()))
+                    let is_nullable = object
+                        .extensions
+                        .get("nullable")
+                        .map(|v| v.as_bool())
+                        .unwrap_or_default()
+                        .unwrap_or_default();
+                    object.metadata.into_iter().map(move |meta| {
+                        (name.clone(), meta, instance.clone(), is_nullable.clone())
+                    })
                 })
-                .flat_map(|(name, meta, instance)| {
-                    meta.description
-                        .into_iter()
-                        .map(move |desc| (name.clone(), desc, instance.clone()))
+                .flat_map(|(name, meta, instance, is_nullable)| {
+                    meta.description.into_iter().map(move |desc| {
+                        (name.clone(), desc, instance.clone(), is_nullable.clone())
+                    })
                 })
-                .map(|(name, desc, instance)| {
+                .map(|(name, desc, instance, is_nullable)| {
                     let parameter = Parameter {
                         description: desc,
                         type_of: instance,
                         is_required: required.contains(&name),
+                        is_nullable,
                     };
 
                     (name, parameter)
@@ -85,6 +91,8 @@ struct Parameter {
     #[serde(rename = "type")]
     type_of: Option<SingleOrVec<InstanceType>>,
     is_required: bool,
+    #[serde(rename = "nullable")]
+    is_nullable: bool,
 }
 
 impl Display for Schema {
