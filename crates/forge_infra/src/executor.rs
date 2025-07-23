185350da67cc5a0c6c10c4bab9b64a15fcf7ace7
laststylebender14 <ran.mahadik14@gmail.com>
2025-07-23 15:56:59 +0530

    - fix tests

--- crates/forge_infra/src/executor.rs
+++ crates/forge_infra/src/executor.rs
@@ -186,6 +186,7 @@ mod tests {
             fetch_truncation_limit: 0,
             stdout_max_prefix_length: 0,
             max_search_lines: 0,
+            max_line_length: 100,
             max_read_size: 0,
             stdout_max_suffix_length: 0,
             http: Default::default(),
